76f56a3d27a0379d9f86a723a24dc4553c1c118b
Aoaoder <2541640782@qq.com>
2024-08-20 23:26:25 +0800

    fix clean sh

--- src/clean.sh
+++ src/clean.sh
@@ -4,6 +4,7 @@
 
 echo "Remove Storage"
 
+sudo chmod -R 777 ./agent/docker-rest-agent/CA_related/storage/fabric-ca-servers
 rm -rf ./agent/docker-rest-agent/storage/*
 
 echo "Remove Fabric CA storage"
