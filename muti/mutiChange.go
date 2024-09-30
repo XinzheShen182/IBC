4adee3404e60dffb0223eaccd7e0aaa6cef64ae5
Aoaoder <2541640782@qq.com>
2024-09-30 23:14:03 +0800

    add muti task

--- muti/mutiChange.go
+++ muti/mutiChange.go
@@ -39,6 +39,10 @@ type ContractInstance struct {
 	InstanceState InstanceState `json:"InstanceState"`
 }
 
+type StateMemory struct {
+	//...原全局变量
+	//需要添加loop中的跳出条件为全局变量
+}
 
 
 type Membership struct {
@@ -57,16 +61,30 @@ type Participant struct {
 	X509          string            `json:"X509"`
 }
 
+
+
+type MutiMessage struct {
+	MutiMessageID        string       `json:"MessageID"`
+	MutiMsgState         ElementState `json:"MsgState"`  //enable/completed
+	MutiType             int          //1-->loop 2-->sequence 3-->parallel
+	MsgList              *string      //存MessageID，loop需要动态添加
+	loopCardinality      int          //顺序/并行-->个数，loop-->次数
+
+    isBefore             bool         //loop专属属性，发消息前/后检测条件
+	loopConditionName    bool         //loop专属属性,循环跳出条件，对应相应全局变量
+
+}
+
 type Message struct {
 	MessageID            string       `json:"MessageID"`
 	MsgState             ElementState `json:"MsgState"`  //disable/enable/completed
-	MiniMsgList          *string      //存minimsgInstanceID 也可以map:minimsginstanceID-->minimsgstate
+	MiniMsgList          *string      //存minimsgInstanceID
 	SendParticipantID    string
 	ReceiveParticipantID string   
 }
 
 type MiniMessage struct {
-	MessageInstanceID    string 
+	MiniMessageID        string 
 	FireflyTranID        string       `json:"FireflyTranID"`
 	MiniMsgState         ElementState `json:"MsgState"`  //enable/wtf/completed
 	Format               string       `json:"Format"`
@@ -135,12 +153,45 @@ func (cc *SmartContract) check_participant(ctx contractapi.TransactionContextInt
 	return true
 }
 
-
-func (cc *SmartContract) Create_mini_message(ctx contractapi.TransactionContextInterface, instanceID string, bigMessageID string) error {
+func (cc *SmartContract) Create_message_ByMuti(ctx contractapi.TransactionContextInterface, instanceID string, MutiMessageID string) error {
 	stub := ctx.GetStub()
 	instance, err := cc.GetInstance(ctx, instanceID)
 
-	msg, err := cc.ReadMsg(ctx, instanceID, "bigMessageID")
+	MutiMsg, err := cc.ReadMsg(ctx, instanceID, "MutiMessageID")
+
+	switch MutiMsg.MutiType {
+	//loop
+	case 1:
+		cc.CreateMessage("message_cccc_0")//只创建一个
+		cc.SetInstance(ctx, instance)
+	
+	case 2:
+		cc.CreateMessage("message_cccc_{0}", ENABLED) //仅对第一个进行ENABLED
+
+		for i := 1; i < MutiMsg.loopCardinality; i++ {
+			cc.CreateMessage("message_cccc_{i}", DISABLED)
+		}
+		
+		cc.SetInstance(ctx, instance)
+
+	case 3:
+		//全部ENABLED
+		for i := 0; i < MutiMsg.loopCardinality; i++ {
+			cc.CreateMessage("message_cccc_{i}", ENABLED)
+		}
+		
+		cc.SetInstance(ctx, instance)
+	default:
+		// 执行默认语句块
+	}
+
+
+
+func (cc *SmartContract) Create_mini_message(ctx contractapi.TransactionContextInterface, instanceID string, MessageID string) error {
+	stub := ctx.GetStub()
+	instance, err := cc.GetInstance(ctx, instanceID)
+
+	msg, err := cc.ReadMsg(ctx, instanceID, "MessageID")
 
 
 	//这一块可以封装为readLocked函数
@@ -270,15 +321,6 @@ func (cc *SmartContract) CreateInstance(ctx contractapi.TransactionContextInterf
     cc.CreateMessage(Message_01k4b43)
 
 
-	//默认创建max个mini消息
-	/*
-	while(i<max){
-		cc.CreateMiniMessage(ctx, &instance, "Message_01k4b43_{instanceID}",发送方i,接收方i)
-	}
-	*/
-
-
-
 	cc.CreateMessage(ctx, &instance, "Message_1f0gefc", "Participant_0oa2za9", "Participant_0jwk4tk", "", DISABLED, `{"properties":{"MessageContent":{"type":"string","description":""},"priceOK":{"type":"boolean","description":""}},"required":["MessageContent","priceOK"],"files":{},"file required":[]}`)
 	cc.CreateGateway(ctx, &instance, "Gateway_1ltys0e", DISABLED)
 
@@ -344,6 +386,11 @@ LockParticipant
 ChangeMsgState
 ChangeSendParticipantMsgListState
 ChangeReceiveParticipantMsgListState
+GetAllminiMessageState
+GetAllMsgState
+...
+...
+
 */
 
 func (cc *SmartContract) Event_0ehnwwz(ctx contractapi.TransactionContextInterface, instanceID string) error {
@@ -380,6 +427,14 @@ func (cc *SmartContract) Message_aaaaa_Send(ctx contractapi.TransactionContextIn
 
 	minimsg, err := cc.ReadminiMsg(ctx, instanceID, "Message_aaaaa", minimsginstanceID)
 	msg, err := cc.ReadMsg(ctx, instanceID, "Message_aaaaa")
+	MutiMsg, err := cc.ReadMutiMsg(ctx, instanceID, "Message_aaaaa")
+
+	
+	if MutiMsg.isBefore == true && MutiMsg.loopConditionName == "xxx"{
+		cc.ChangeMutiMsgState()
+		return
+	}
+
 
 	if err != nil {
 		return err
@@ -438,6 +493,7 @@ func (cc *SmartContract) Message_aaaaa_Confirm(ctx contractapi.TransactionContex
 
 	minimsg, err := cc.ReadMiniMsg(ctx, instanceID, "Message_aaaaa", minimsginstanceID)
 	msg, err := cc.ReadMsg(ctx, instanceID, "Message_aaaaa")
+	MutiMsg, err := cc.ReadMutiMsg(ctx, instanceID, "Message_aaaaa")
 
 	if err != nil {
 		return err
@@ -478,6 +534,41 @@ func (cc *SmartContract) Message_aaaaa_Confirm(ctx contractapi.TransactionContex
 	//完成单个小消息
 	cc.ChangeMiniMsgState(ctx, instance, minimsg.minimsgInstanceID, COMPLETED)
 	stub.SetEvent("Message_aaaaa_{InstanceID}", []byte("Message has been COMPLETED"))
+
+	//检查所有小消息，是否完成中消息
+	if GetAllminiMessageState("Message_aaaaa") == true {
+
+		cc.ChangeMsgState(ctx, instance, "Message_aaaaa", COMPLETED)
+
+		if MutiMsg.isBefore == true {
+			return
+		}
+
+		//分情况检查所有中消息，是否完成大消息
+		switch MutiMsg.MutiType {
+		case 1:
+			if len(MutiMsg.MsgList)< MutiMsg.loopCardinality  && MutiMsg.loopConditionName != "xxx"{
+				cc.CreateMessage()
+				cc.addMsgList()
+			}else if len(MutiMsg.MsgList) ==  MutiMsg.loopCardinality || MutiMsg.loopConditionName == "xxx"{
+				cc.ChangeMutiMsgState()
+			}
+
+		case 2:
+			if len(MutiMsg.MsgList)< MutiMsg.loopCardinality{
+				cc.CreateMessage()
+				cc.addMsgList()
+			}else if len(MutiMsg.MsgList) ==  MutiMsg.loopCardinality{
+				cc.ChangeMutiMsgState()
+			}
+			//由于1，2有顺序，可以只检查len
+
+		case 3:
+			if GetAllMsgState("Message_aaaaa") == true{
+				cc.ChangeMutiMsg()
+			}
+		}
+	}
 	cc.SetInstance(ctx, instance)
 	
 	return nil
@@ -516,4 +607,3 @@ func (cc *SmartContract) Message_aaaaa_Advance(ctx contractapi.TransactionContex
 
 
 //TODO：任务muti 可以参考使用 建模参数 
-
